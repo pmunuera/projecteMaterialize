6d0da79f33d8a8907fed805bf4f6c379bd1f9bc6
pablo <pmunueragarcia.cf@iesesteveterradas.cat>
2023-02-03 18:52:19 +0100

    first commit

--- www/js/init.js
+++ www/js/init.js
@@ -1,15 +1,15 @@
 (function($){
   $(function(){
-    
+    $('.sidenav').sidenav();
+    $('.tabs').tabs({"swipeable":true});
+    //$('#tabs-swipe-demo').tabs("select", "tab2");
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-
 document.addEventListener('deviceready', onDeviceReady, false);
-
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-
+  
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     //document.getElementById('deviceready').classList.add('ready');
 }
