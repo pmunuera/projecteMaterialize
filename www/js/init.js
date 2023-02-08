a27ce973d0dcfc8ad1ac526f0d8c7298d53be61e
pablo <pmunueragarcia.cf@iesesteveterradas.cat>
2023-02-08 21:31:45 +0100

    first commit

--- www/js/init.js
+++ www/js/init.js
@@ -2,7 +2,8 @@
   $(function(){
     $('.sidenav').sidenav();
     $('.tabs').tabs({"swipeable":true});
-    //$('#tabs-swipe-demo').tabs("select", "tab2");
+    
+    $('#loadArticlesButton').click(load_articles);
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
@@ -12,4 +13,38 @@ function onDeviceReady() {
   
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     //document.getElementById('deviceready').classList.add('ready');
+}
+
+function load_articles(){
+  $.ajax({
+    method: "GET",
+    url: "https://api.spaceflightnewsapi.net/v3/articles?_limit=3",
+    dataType: "json",   // necessitem això pq ens retorni un objecte JSON
+  }).done(function (msg) {
+    for(let item in msg) {
+      var title = msg[item].title;
+      var elem = $("<li class='collection-item'><div id='"+msg[item].id+"'>"+title+"<a id='sendTo2' href='#!' class='secondary-content'><i class='material-icons'>send</i></a></div></li>");
+      $('#sendTo2',elem).click(loadInformation);
+      $('#articleList').append(elem);
+    };
+  }).fail(function () {
+    alert("ERROR");
+  });
+}
+function loadInformation(e){
+  var idArticle=$(e.target).parent().parent().attr("id");
+  alert(idArticle);
+  $.ajax({
+    method: "GET",
+    url: "https://api.spaceflightnewsapi.net/v3/articles/"+idArticle,
+    dataType: "json",   // necessitem això pq ens retorni un objecte JSON
+  }).done(function (msg) {
+    for(let item in msg) {
+        var title = msg[item].title;
+        $('#test-swipe-2').append("<p>"+title+"</p>");
+    };
+  }
+  ).fail(function () {
+    alert("ERROR");
+  });
 }
